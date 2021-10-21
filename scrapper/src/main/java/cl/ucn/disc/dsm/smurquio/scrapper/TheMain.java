57b38591441518aa02d577b8937c3918f695bd9e
NekoDarkZ <sebastian.murquio@alumnos.ucn.cl>
2021-10-21 20:04:20 -0300

    scrapper implemented

--- scrapper/src/main/java/cl/ucn/disc/dsm/smurquio/scrapper/TheMain.java
+++ scrapper/src/main/java/cl/ucn/disc/dsm/smurquio/scrapper/TheMain.java
@@ -2,6 +2,7 @@ package cl.ucn.disc.dsm.smurquio.scrapper;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
@@ -9,9 +10,11 @@ import org.jsoup.nodes.Document;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -31,51 +34,81 @@ public class TheMain {
      *The starting point.
      *@param args to use.
      **/
-    public static void main(String[] args) throws IOException {
-        log.debug("Starting the Scrapping...");
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-        //The id
-        int id = 142;
+        log.debug("Loading the Funcionarios from funcionarios.json ...");
 
-        //Connect and get the Document
-        Document doc = Jsoup
+        //Load the file into the data
+        String data = FileUtils.readFileToString(new File("funcionarios.json"), StandardCharsets.UTF_8);
+
+        //Define the Type
+        Type type = new TypeToken<List<Funcionario>>(){
+        }.getType();
+
+        //The List of Funcionario (string -> List<Funcionario>)
+        List<Funcionario> funcionarios = GSON.fromJson(data, type);
+
+        //The latest id loaded
+        int start = funcionarios.get(funcionarios.size() -1).getId();
+        int end = 30000;
+
+        Random r = new Random();
+        log.debug("Starting the Scrapping from {} to {} ...", start, end);
+        for(int id = start; id <= end; id++) {
+
+            //Wait for...
+            Thread.sleep(50 + r.nextInt(350));
+
+            log.debug("Retriving Funcionario id: {}.", id);
+
+            //Connect and get the Document
+            Document doc = Jsoup
                 .connect("https://admision01.ucn.cl/directoriotelefonicoemail/fichaGenerica/?cod=" + id)
                 .get();
 
-        String nombre = doc.getElementById("lblNombre").text();
-        String cargo = doc.getElementById("lblCargo").text();
-        String unidad = doc.getElementById("lblUnidad").text();
-        String email = doc.getElementById("lblEmail").text();
-        String telefono = doc.getElementById("lblTelefono").text();
-        String oficina = doc.getElementById("lblOficina").text();
-        String direccion = doc.getElementById("lblDireccion").text();
+            String nombre = doc.getElementById("lblNombre").text();
+            String cargo = doc.getElementById("lblCargo").text();
+            String unidad = doc.getElementById("lblUnidad").text();
+            String email = doc.getElementById("lblEmail").text();
+            String telefono = doc.getElementById("lblTelefono").text();
+            String oficina = doc.getElementById("lblOficina").text();
+            String direccion = doc.getElementById("lblDireccion").text();
 
-        //Call the constructor
-        Funcionario f = Funcionario.builder()
-            .id(id)
-            .nombre(nombre)
-            .cargo(cargo)
-            .unidad(unidad)
-            .email(email)
-            .telefono(telefono)
-            .oficina(oficina)
-            .direccion(direccion)
-            .build();
+            //Skip if data not found
+            if(nombre.length() <= 1){
+                log.warn("No data found for id: {}.", id);
+                continue;
+            }
 
-        //The List of Funcionario
-        List<Funcionario> funcionarios = new ArrayList<>();
+            log.info("Funcionario id: {} founded: {}.", id, nombre);
 
-        //Add the Funcionario into the list
-        funcionarios.add(f);
+            //Call the constructor
+            Funcionario f = Funcionario.builder()
+                .id(id)
+                .nombre(nombre)
+                .cargo(cargo)
+                .unidad(unidad)
+                .email(email)
+                .telefono(telefono)
+                .oficina(oficina)
+                .direccion(direccion)
+                .build();
 
-        //Add the Funcionario in JSON Format
-        FileUtils.writeStringToFile(
-            new File("funcionarios.json"),
-            GSON.toJson(funcionarios),
-            StandardCharsets.UTF_8);
+            //Add the Funcionario into the list
+            funcionarios.add(f);
 
+            //Save by 25
+            if(funcionarios.size() % 3 == 0){
 
-        log.debug("Nombre: {}, Cargo: {}",nombre,cargo);
+                log.debug("Writting {} Funcionarios to file...", funcionarios.size());
+
+                //Write the List of Funcionario in JSON Format
+                FileUtils.writeStringToFile(
+                    new File("funcionarios.json"),
+                    GSON.toJson(funcionarios),
+                    StandardCharsets.UTF_8);
+            }
+        }
         log.debug("Done.");
     }
 
