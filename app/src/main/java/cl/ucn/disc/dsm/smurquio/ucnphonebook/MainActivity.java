29f55c25971bd5d7ea7b4805adcba81627fff581
NekoDarkZ <sebastian.murquio@alumnos.ucn.cl>
2021-10-21 22:20:18 -0300

    build(graddle): Adding ACRA and LeakCanary
    Adding ACRA and LeakCanary dependencies to build.graddle and App.java was added with the ACRA configuration.

--- app/src/main/java/cl/ucn/disc/dsm/smurquio/ucnphonebook/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/smurquio/ucnphonebook/MainActivity.java
@@ -6,9 +6,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     *
+     * @param savedInstanceState the state.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Se genero un error
+        //throw new IllegalArgumentException("Error en la aplicacion");
     }
 }
